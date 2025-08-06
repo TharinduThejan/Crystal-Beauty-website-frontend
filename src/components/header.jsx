dbb58c1828019c52a80fffd5265806147a3bad5a
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-08-06 14:04:41 +0530

    add header components

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+import UserData from "./userData";
+
+export default function Header() {
+  return (
+    <div className="bg-[#ffff00]">
+      <h1 className="text-[30px]">Crystal Beauty Clear</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam
+        necessitatibus iusto, similique dolor laboriosam assumenda sequi dolorum
+        velit quod? Numquam magni sunt quos nihil adipisci ut ipsam eum dolore
+        soluta.
+      </p>
+      <UserData />
+    </div>
+  );
+}
---
+++ src/components/userData.jsx
@@ -0,0 +1,8 @@
+export default function UserData() {
+  return (
+    <div>
+      <h2>User Data</h2>
+      <button>Logout</button>
+    </div>
+  );
+}
