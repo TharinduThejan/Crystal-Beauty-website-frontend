e64dd1a83f0846f6a5d8e5e6c1fae9f3f208f199
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-08-18 00:26:06 +0530

    add useNavigation ▶️

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,21 +1,37 @@
 import axios from "axios";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
   //e-mail and password are dynamically changing values
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   async function handleLogin() {
     console.log("Email:", email);
     console.log("Password:", password);
     try {
-      const response = await axios.post("http://localhost:3000/users/login", {
-        email,
-        password,
-      });
+      const response = await axios.post(
+        import.meta.env.VITE_BACKEND_URL + "/api/users/login", //sent backend api URI to .env
+        {
+          email,
+          password,
+        }
+      );
       toast.success("Login successful!");
       console.log(response.data);
+
+      localStorage.setItem("token", response.data.token); //save data in local storage
+
+      if (response.data.role === "admin") {
+        // window.location.href = "/admin";
+        navigate("/admin"); //if user===admin then navigate to admin
+      } else {
+        // window.location.href = "/";
+        navigate("/"); //else navigate to home
+      }
     } catch (error) {
       toast.error(error.response.data.message);
     }
@@ -46,7 +62,7 @@ export default function Login() {
 
           <button
             onClick={handleLogin}
-            className="w-[300px] h-[50px] border bg-[#c3efe9] text-white rounded-[20px] my-[20px]"
+            className="w-[300px] h-[50px] border border-white bg-[#c3efe9] text-black rounded-[20px] my-[20px] cursor-pointer hover:bg-[#b2e6e0] transition duration-300"
           >
             Login
           </button>
