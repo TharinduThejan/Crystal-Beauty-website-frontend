6a2ed415c9d5b08b530832f6ad6d3203e9c5eda4
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-09-04 20:21:17 +0530

    upgrade header and home page by adding styles and functions 💪😁

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,46 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import UserData from "./userData";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
   return (
-    <div className="bg-lime-500">
-      <h1 className="text-[30px]">Crystal Beauty Clear</h1>
-      {/* <a href="/">Home</a>
-      <a href="/login">Login</a> */}
-      {/* //link tag is very smooth.no page reload */}
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/signup">Sign Up</Link>
-    </div>
+    <header className="w-full shadow-lg  flex items-center justify-between">
+      <img
+        onClick={() => navigate("/")}
+        src="/logo.png"
+        alt="Logo"
+        className="h-20 object-cover cursor-pointer"
+      />
+      <nav className="w-[calac(100%-160px)]h-full">
+        <Link
+          to="/"
+          className="text-black font-medium px-4 py-2  hover:bg-white hover:text-emerald-600 transition duration-200 "
+        >
+          Home
+        </Link>
+        <Link
+          to="/product"
+          className="text-black font-medium px-4 py-2  hover:bg-white hover:text-emerald-600 transition duration-200 "
+        >
+          Product
+        </Link>
+        <Link
+          to="/about"
+          className="text-black font-medium px-4 py-2  hover:bg-white hover:text-emerald-600 transition duration-200 "
+        >
+          About
+        </Link>
+        <Link
+          to="/contact"
+          className="text-black font-medium px-4 py-2  hover:bg-white hover:text-emerald-600 transition duration-200 "
+        >
+          Contact
+        </Link>
+      </nav>
+      <div className="w-[80px] bg-blue-500">
+        <UserData />
+      </div>
+    </header>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "../components/header";
+import Product from "./client/productPage";
+
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-6 text-white">
-      <h2 className="text-4xl font-extrabold mb-4 drop-shadow-lg">
-        Welcome to the Home Page
-      </h2>
-      <p className="text-lg max-w-xl text-center drop-shadow-md">
-        This is the main content area.
-      </p>
+    <div className="min-h-screen flex flex-col items-center  bg-gradient-to-r from-white-500 text-black p-8">
+      <Header />
+      <div className="bg-amber-500 w-full h-[calc(100vh-80px)] flex flex-col items-center">
+        <Routes path="/">
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/about" element={<h1>About Page</h1>} />
+          <Route path="/contact" element={<h1>Contact Page</h1>} />
+          <Route path="/*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </div>
     </div>
   );
 }
